b096abc9bb619cacf4f17814c96b6748920bf2a1
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-12-29 08:12:19 +0530

    implement flatList

--- App.js
+++ App.js
@@ -22,16 +22,19 @@ export default class App extends Component {
   handlePlaceAdded = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({ 
+          key: Math.random(), 
+          value: placeName 
+        })
       };
     });
   };
 
-  handlePlaceDeleted = index => {
+  handlePlaceDeleted = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
